d11af34b969b8c9fe46280d3d6905a2b0b52e028
hannahchapman79 <hannahchapman1999@aol.com>
2024-12-12 10:47:03 +0700

    Protects delete user route using JWT authorisation and seperates JWT tests into new file

--- __tests__/endpoints/app.test.js
+++ __tests__/endpoints/app.test.js
@@ -6,6 +6,8 @@ const app = require("../../app")
 const endpoints = require("../../endpoints.json")
 const jwt = require("jsonwebtoken");
 
+jest.mock("../../middleware/jwtAuth", () => (request, response, next) => next());
+
 beforeEach(() => seed(data));
 
 afterAll(() => db.end());
@@ -187,14 +189,6 @@ describe("/api/users", () => {
     });
   });
   describe("DELETE", () => {
-    test("responds with 200 status and deletes the user by user id", () => {
-      return request(app)
-        .delete("/api/users/1")
-        .expect(200)
-        .then((response) => {
-          expect(response.body.message).toEqual("User successfully deleted");
-        });
-    });
     test("responds with 404 error when the given user id doesn't exist in the database", () => {
       return request(app)
         .delete("/api/users/800")
@@ -214,65 +208,3 @@ describe("/api/users", () => {
   });
 })
 
-describe("/api/login", () => {
-  describe("POST", () => {
-    test("200: Logs in a user with valid credentials and returns a JWT", async () => {
-      const userSignup = {
-        username: "orangecat",
-        password: "football000",
-        email: "orangecat@gmail.com",
-      };
-      const loginDetails = {
-        email: "orangecat@gmail.com",
-        password: "football000",
-      };
-
-      await request(app)
-        .post("/api/users")
-        .send(userSignup)
-        .expect(201)
-        .then(({ body }) => {
-          const { user } = body;
-          expect(user).toHaveProperty("username", "orangecat");
-          expect(user).toHaveProperty("email", "orangecat@gmail.com");
-          expect(user).not.toHaveProperty("password");
-        });
-
-      return request(app)
-        .post("/api/users/login")
-        .send(loginDetails)
-        .expect(200)
-        .then(({ body }) => {
-          const { token, user } = body;
-          expect(user).toHaveProperty("username", "orangecat");
-          expect(user).toHaveProperty("email", "orangecat@gmail.com");
-          expect(user).not.toHaveProperty("password");
-
-          expect(token).toBeDefined();
-
-          const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-          expect(decoded).toMatchObject({
-            user_id: expect.any(Number),
-            username: "orangecat",
-            email: "orangecat@gmail.com",
-          });
-
-          expect(decoded.exp).toBeGreaterThan(Math.floor(Date.now() / 1000));
-        });
-    });
-    test("400: Returns error if email or password are incorrect", async () => {
-      const loginDetails = {
-        email: "nonexistentuser@gmail.com",
-        password: "wrongpassword",
-      };
-
-      return request(app)
-        .post("/api/users/login")
-        .send(loginDetails)
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.message).toEqual("Bad email or password");
-        });
-    });
-  });
-});
