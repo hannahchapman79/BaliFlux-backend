2114e353f2fe10725d770a999b8534153dd4b1cb
hannahchapman79 <hannahchapman1999@aol.com>
2024-12-16 11:43:48 +0700

    MongoDB installation and adds questionnaire data to store survey questions

---
+++ db/data/questionsData.js
@@ -0,0 +1,72 @@
+const questionsData = [
+    {
+        question: "What type of experiences do you prefer?",
+        answers: [
+            "Beaches",
+            "Spa & Wellness",
+            "Nightlife",
+            "Culture & Temples",
+            "Jungle & Nature",
+            "Adventure Sports",
+            "Hidden Gems",
+            "Surfing",
+            "Shopping",
+            "Fitness & Gyms"
+        ],
+    },
+    {
+        question: "What type of food experiences excite you?",
+        answers: [
+            "Local Cuisine",
+            "Fine Dining",
+            "Beach Clubs",
+            "Brunch Spots",
+            "Vegan & Healthy Eats",
+            "Coffee Culture",
+            "Unique Dining",
+            "Any top rated - I'm a foodie!"
+        ],
+    },
+    {
+        question: "What vibe are you looking for?",
+        answers: [
+            "Relaxed",
+            "Social & Lively",
+            "Romantic",
+            "Adventurous",
+            "Cultural Immersion",
+            "Focused & Productive",
+            "Spiritual & Serene",
+            "Family-Friendly"
+        ],
+    },
+    {
+        question: "What nature spots would you like to explore?",
+        answers: [
+            "Beaches & Coastlines",
+            "Mountains & Volcanos",
+            "Waterfalls",
+            "Rice Terraces",
+            "Jungle",
+            "Marine Life"
+        ],
+    },
+    {
+        question: "What's your travel style?",
+        answers: [
+            "Solo Adventures",
+            "Budget-friendly",
+            "Luxury Travel",
+            "Honeymoon/Couple",
+            "Guided Tours",
+            "Friends Trip"
+        ],
+    },
+    {
+        question: "How many days is your trip?",
+        answers: null,
+        type: "input",
+    }
+];
+
+module.exports = questionsData;
