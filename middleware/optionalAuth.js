2f818118e088dffdccd0e5babf88e2b597205134
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-22 12:06:51 +0700

    Adds optional JWT authentication as guest users can generate itinerary

---
+++ middleware/optionalAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+function optionalAuth(request, response, next) {
+    const authHeader = request.headers.authorization || request.headers.Authorization;
+    
+    if (!authHeader?.startsWith('Bearer ')) {
+        request.isGuest = true;
+        return next();
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        request.userId = decoded.userId;
+        request.isGuest = false;
+        next();
+    } catch (error) {
+        request.isGuest = true;
+        next();
+    }
+}
+
+module.exports = optionalAuth;
--- routes/itinerary.routes.js
+++ routes/itinerary.routes.js
@@ -1,12 +1,13 @@
 const express =require("express");
 const { postItinerary, getItineraryByUserId } = require("../controllers/itinerary.controller")
 const verifyToken = require("../middleware/jwtAuth")
+const optionalAuth = require("../middleware/optionalAuth")
 
 const itineraryRouter = express.Router();
 
 itineraryRouter
     .route("/")
-    .post(postItinerary)
+    .post(optionalAuth, postItinerary)
 
 itineraryRouter
     .route("/:userId")
